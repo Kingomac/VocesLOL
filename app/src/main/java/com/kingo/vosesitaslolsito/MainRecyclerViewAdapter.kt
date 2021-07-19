a666214d8dc265b3eee89c270dd352f89d53ef3f
Mario <kingomacyt@gmail.com>
2021-07-19 15:50:41 +0200

    Nuevas skins y campeones

--- app/src/main/java/com/kingo/vosesitaslolsito/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/kingo/vosesitaslolsito/MainRecyclerViewAdapter.kt
@@ -42,9 +42,27 @@ class MainRecyclerViewAdapter(private val dataSet: Array<Champ>, private val app
             Champ.AHRI -> R.drawable.ahri_circle
             Champ.AHRI_SKIN_14 -> R.drawable.ahri_circle_14
             Champ.AHRI_SKIN_27 -> R.drawable.ahri_circle_27_pie_c_10_16
+            Champ.AHRI_SKIN_28 -> R.drawable.ahri_circle_28_pie_c_10_22
             Champ.AKALI -> R.drawable.akali_circle
+            Champ.AKALI_SKIN_9 -> R.drawable.akali_circle_9
             Champ.AKALI_SKIN_14 -> R.drawable.akali_circle_14
+            Champ.AKALI_SKIN_15 -> R.drawable.akali_circle_15_pie_c_9_22
+            Champ.AKALI_SKIN_32 -> R.drawable.akali_circle_32_pie_c_10_22
             Champ.JHIN_SKIN_5 -> R.drawable.jhin_circle_5
+            Champ.JHIN_SKIN_1 -> R.drawable.jhin_circle_1
+            Champ.JHIN_SKIN_4 -> R.drawable.jhin_circle_4
+            Champ.SENNA -> R.drawable.senna_circle_0_senna
+            Champ.SENNA_SKIN_1 -> R.drawable.senna_circle_1_senna
+            Champ.SENNA_SKIN_10 -> R.drawable.senna_circle_10_pie_c_10_12
+            Champ.SENNA_SKIN_16 -> R.drawable.senna_circle_16_pie_c_legacy_bugs_2021
+            Champ.EVELYNN -> R.drawable.evelynn_circle
+            Champ.EVELYNN_SKIN_6 -> R.drawable.evelynn_circle_6
+            Champ.EVELYNN_SKIN_15 -> R.drawable.evelynn_circle_15_pie_c_10_22
+            Champ.EZREAL -> R.drawable.ezreal_circle
+            Champ.EZREAL_SKIN_5 -> R.drawable.ezreal_circle_5
+            Champ.EZREAL_SKIN_9 -> R.drawable.ezreal_circle_9
+            Champ.EZREAL_SKIN_18 -> R.drawable.ezreal_circle_18
+            Champ.EZREAL_SKIN_21 -> R.drawable.ezreal_circle_21
             else -> R.drawable.ic_launcher_foreground
         }, 0, 0)
     }
